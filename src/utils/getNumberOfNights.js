c6e19d6e8d58cb891726d7a7ceec94144e920157
Viorel Lazari <vlazari@gmail.com>
2023-05-26 16:22:07 +0200

    clean up

--- src/utils/getNumberOfNights.js
+++ src/utils/getNumberOfNights.js
@@ -1,3 +1,9 @@
+/**
+ * The function calculate the number of nights between the two given days.
+ * @param {new Date} startDay
+ * @param {new Date} endDay
+ * @returns the number of nights or null.
+ */
 export default function getNumberOfNights(startDay, endDay) {
 	if (startDay && endDay) {
 		const timeDiff = Math.abs(endDay.getTime() - startDay.getTime());
